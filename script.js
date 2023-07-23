8bcf2a4e92f9f66ea0a3a2d190487f19423bff72
Nicky <raccoonutau@gmail.com>
2023-07-22 20:29:23 -0500

    Filled in the rest of the empty functions, fixed details button
    Added the remaining functions, created the HTML for "add player" card and player cards, details were given a "hidden" element that will be removed when details button is clicked

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
-
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
+const cohortName = '2306-FTB-ET-WEB-FT';
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+/**
+ * It fetches all players from the API and returns them
+ * @returns An array of objects.
+ */
 const fetchAllPlayers = async () => {
   try {
     const response = await fetch(APIURL + 'players');
@@ -12,6 +17,7 @@ const fetchAllPlayers = async () => {
   }
 };
 
+//Button that unhides the details with the 'hidden' class when clicked on
 const togglePlayerDetails = (playerCard) => {
   const detailsBtn = playerCard.querySelector('.details-btn');
   const breedInfo = playerCard.querySelector('.breed-info');
@@ -27,7 +33,6 @@ const fetchSinglePlayer = async (playerId) => {
   try {
     const response = await fetch(APIURL + `players/${playerId}`);
     const data = await response.json();
-    showPlayerDetailsModal(data.data);
   } catch (err) {
     console.error(`Oh no, trouble fetching player #${playerId}!`, err);
   }
@@ -85,31 +90,86 @@ const removePlayer = async (playerId) => {
  * @returns the playerContainerHTML variable.
  */
 const renderAllPlayers = (playerList) => {
-    try {
-        
-    } catch (err) {
-        console.error('Uh oh, trouble rendering players!', err);
+  try {
+    playerContainer.innerHTML = ''; 
+    // Clears current players before rendering new ones
+
+    for (const player of playerList) {
+      // Create a new div element to hold the player information
+      const playerCard = document.createElement('div');
+      playerCard.classList.add('player-card');
+
+      // HTML for the player cards
+      playerCard.innerHTML = `
+        <img src="${player.imageUrl}" alt="${player.name}">
+        <h3>${player.name}</h3>
+        <p class="breed-info hidden">Breed: ${player.breed}</p>
+        <p class="status-info hidden">Status: ${player.status}</p>
+        <button class="details-btn">See Details</button>
+        <button class="remove-btn" data-id="${player.id}">Remove from Roster</button>
+      `;
+
+      // Add event listeners for the "See Details" and "Remove from Roster"
+      const detailsBtn = playerCard.querySelector('.details-btn');
+      detailsBtn.addEventListener('click', () => {
+        fetchSinglePlayer(player.id);
+      });
+
+      const removeBtn = playerCard.querySelector('.remove-btn');
+      removeBtn.addEventListener('click', () => {
+        removePlayer(player.id);
+      });
+
+      // Add event listener to the "See Details" button
+      togglePlayerDetails(playerCard);
+
+      // Append player card to the playerContainer
+      playerContainer.appendChild(playerCard);
     }
+  } catch (err) {
+    console.error('Uh oh, trouble rendering players!', err);
+  }
+};
+//HTML for the form to make a new player
+const renderNewPlayerForm = () => {
+  try {
+    newPlayerFormContainer.innerHTML = `
+      <h2>Add New Player</h2>
+      <h5>The new player will be the last in the lineup<h5>
+      <input type="text" id="player-name" placeholder="Name" />
+      <input type="text" id="player-breed" placeholder="Breed" />
+      <input type="text" id="player-image" placeholder="Image URL" />
+      <button id="add-player-btn">Add Player</button>
+    `;
+
+    const addPlayerBtn = document.getElementById('add-player-btn');
+    addPlayerBtn.addEventListener('click', () => {
+      const playerName = document.getElementById('player-name').value;
+      const playerBreed = document.getElementById('player-breed').value;
+      const playerImage = document.getElementById('player-image').value;
+
+      const newPlayerObj = {
+        name: playerName,
+        breed: playerBreed,
+        imageUrl: playerImage,
+        status: 'bench', // New players are added to the bench status by default
+      };
+
+      addNewPlayer(newPlayerObj);
+    });
+  } catch (err) {
+    console.error('Uh oh, trouble rendering the new player form!', err);
+  }
 };
 
-
-/**
- * It renders a form to the DOM, and when the form is submitted, it adds a new player to the database,
- * fetches all players from the database, and renders them to the DOM.
- */
-const renderNewPlayerForm = () => {
-    try {
-        
-    } catch (err) {
-        console.error('Uh oh, trouble rendering the new player form!', err);
-    }
-}
-
 const init = async () => {
+  try {
     const players = await fetchAllPlayers();
     renderAllPlayers(players);
-
     renderNewPlayerForm();
-}
+  } catch (err) {
+    console.error('Error initializing the app:', err);
+  }
+};
 
 init();
